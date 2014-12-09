d176fdc83351d84795e8f585b8571a41485af991
harshkn <harshkn@gmail.com>
2014-12-09 01:43:11 +0100

    Added P, Q and R matrix generation.
    Added P, Q and R matrix generation. and updated N matrix generation

--- t9functions.py
+++ t9functions.py
@@ -19,11 +19,14 @@ def readTextFile(file):
 #######################################################################
 
 
-#get transition probability into a 26x26 matrix - N
+#get N matrix H x H = 26x26 Transition matrix
+#get R matrix = H x 1 = 26 x 1 Initial Matrix
+#Choosing how each word starts i.e transition from space to character
 
-def getNMatrix(text):
+def getNandRMatrix(text):
         import numpy
         N = numpy.zeros((26, 26))
+        R = numpy.zeros((26,1))
         sp = 0;
         for i, c in enumerate(text[:-1]) :
                 cval = ord(c)
@@ -32,19 +35,54 @@ def getNMatrix(text):
                         m = cval - 97;
                         n = cvalp - 97;
                         N[m,n] = N[m,n] + 1
-                elif (cval ==32) | (cvalp == 32) :
-                        sp = sp + 1                       
+                elif (cval ==32) & (cvalp >=97) & (cvalp <= 122):
+                        n = cvalp - 97;
+                        R[n] = R[n] + 1
+                        sp = sp + 1
+                elif (cvalp == 32) :
+                        sp = sp
                 else:
                         print(c)
                         print("error")
         #Normalise N
         denN = numpy.sum(N, axis = 1)
         NormN = N / denN[:,None]
+
+        NormR = R/(numpy.sum(R))
 ##        print(numpy.sum(NormN, axis = 1))
 ##        print(denN)
 ##        print(N[25,:]/sum(N[25,:]))
-        return NormN, sp
+        return NormN, NormR
 
+#######################################################################
+
+#get Q matrix = V x H = 8 x 26 Emmission Matrix
+#Choosing equal probability for each character given a key is pressed
+def getQMatrix():
+        import numpy
+        Q = numpy.zeros((8,26))
+        #2, 3 ,4 ,5 ,6, 7,  8, 9    <- V
+        #ABCDEFGHIJKLMNOPQRSTUVWXYZ <- H
+        #012345678901234567890123456
+        
+        Q[0,0:3] = 1   #2, ABC
+        Q[1,3:6] = 1   #3, DEF
+        Q[2,6:9] = 1   #4, GHI
+        Q[3,9:12] = 1  #5, JKL
+        Q[4,12:15] = 1 #6, MNO
+        Q[5,15:19] = 1 #7, PQRS
+        Q[6,19:22] = 1 #8, TUV
+        Q[7,22:26] = 1 #9, WXYZ
+        #space is not considered.
+        denQ = numpy.sum(Q, axis = 1)
+        NormQ = Q / denQ[:,None]
+        return NormQ
+
+######################################################################
+
+
+        
+        
                 
         
         
